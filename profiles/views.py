38ed5b5caf48ac94d84a953da8310220358eef83
allan zizinga <allanzizinga@yahoo.com>
2024-08-28 19:25:02 +0200

    added profiles detail view

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-
+from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -10,3 +11,36 @@ class ProfileList(APIView):
         profiles=Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
+    
+class ProfileDetail(APIView):
+    """
+    Getting a profile.
+    """
+    """
+    serializer_class = ProfileSerializer/This helps us to automatically add a form with the fields already stated.
+    """
+    serializer_class = ProfileSerializer
+
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+   
+    def put(self, request,pk):
+        """
+        Updating a profile
+        """
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
