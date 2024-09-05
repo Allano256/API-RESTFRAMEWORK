30e15ee10c644529d4f2f6b14e040783e06cd734
allan zizinga <allanzizinga@yahoo.com>
2024-09-05 05:10:27 +0200

    Created the Likes resource

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from likes.models import Likes
+from likes.serializer import LikesSerializer
 
 # Create your views here.
+class LikeList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikesSerializer
+    queryset = Likes.objects.all()
+
+    def perform_create(self, serializer):
+        """
+        This will ensure that the like is made by the signed in user only.
+        """
+        serializer.save(owner=self.request.user)
+
+class LikeDetail(generics.RetrieveAPIView):
+    # Only the user that liked the post can un-like it
+    permission_classes=[IsOwnerOrReadOnly]
+    serializer_class = LikesSerializer
+    queryset = Likes.objects.all()
+
